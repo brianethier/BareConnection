69f1b76360b9431d167050bc9d2b8c7f9883f114
Brian Ethier <brian.ethier@gmail.com>
2016-03-15 21:25:36 -0400

    Adding RestUtils helper method to easily combine path segments into a single path.

--- src/ca/barelabs/bareconnection/RestUtils.java
+++ src/ca/barelabs/bareconnection/RestUtils.java
@@ -6,7 +6,23 @@ import java.util.Map;
 import java.util.Set;
 
 
-public class RestUtils {    
+public class RestUtils {
+
+	public static String toPath(String... paths) {
+		StringBuilder sb = new StringBuilder();
+        if (paths != null) {
+        	for (String path : paths) {
+        		if (path == null) {
+        			continue;
+        		}
+                if (sb.length() > 0) {
+                    sb.append(RestConnection.PATH_SEPARATOR);
+                }
+                sb.append(path);
+        	}
+        }
+        return sb.toString();
+	}
     
     public static String toQuery(Map<?, ?> params, String charset) throws UnsupportedEncodingException {
         StringBuilder sb = new StringBuilder();
